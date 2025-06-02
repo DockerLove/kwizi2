5505fe71ffb07d812ed85ecdc98fe5d9856baeec
Егор Труфанов <neonuar2005@gmail.com>
2025-06-02 16:51:02 +0300

    New Project, delete Authen and User services

---
+++ kwizi/src/main/java/com/example/kwizi/KwiziApplication.java
@@ -0,0 +1,13 @@
+package com.example.kwizi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwiziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwiziApplication.class, args);
+	}
+
+}
---
+++ kwizi/src/test/java/com/example/kwizi/KwiziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kwizi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwiziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
